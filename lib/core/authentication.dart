616d1ac492b726ebab66034cd146a56385ff5dfe
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-18 20:29:10 +0200

    applesignin

--- lib/core/authentication.dart
+++ lib/core/authentication.dart
@@ -2,4 +2,9 @@ part of data.core;
 
 class Authentication extends UnitAuthentication {
   // Authentication({name, options}) : super(name: name, options: options);
+  Authentication()
+      : super(
+          appleServiceId: 'com.zaideih.app.signin',
+          redirectUri: 'https://zaideih-app.firebaseapp.com/__/auth/handler',
+        );
 }
